48f0dabd7a1b065b0e15e7099ac9a5de77b56daf
brunojular <brunojular00@gmail.com>
2022-05-08 20:09:55 -0300

    footer con términos y condiciones, funcionalidad de los botones en las explicaciones, funcionalidad botón contacto, botón flotante wp, regulación de tamaño de tipografias, animaciones sarpadas

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from "@angular/core"
+import { delay } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,65 @@ import { Component, ElementRef, ViewChild } from "@angular/core"
 export class AppComponent {
   title = 'joaquin-web';
 
+  background = '../assets/homeBackground/1.png';
+  backgroundN = 1;
+
+  termsHidden=true;
+
   constructor() {
   }
+  
+  ngOnInit() {
+    // await this.delay();
+    window.addEventListener("scroll", this.reveal);
+
+    // To check the scroll position on page load
+    this.reveal();
+  }
+
+
   goTo(url: any) {
     window.open(url, "_blank");
-
   }
+
   onSwiper(swipe: any) {
     console.log('asd');
   }
+
   onSlideChange() {
     console.log('slide change');
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth'});
+  }
+
+  wp() {
+    // open whatsapp and send message
+    window.open('https://wa.me/+5493512613917?text=Im%20interested%20in%20your%20car%20for%20sale', '_blank');
+  }
+  
+  delay() {
+    setTimeout (() => {
+      console.log("Hello from setTimeout");
+      this.backgroundN += 1;
+      if(this.backgroundN == 3) {this.backgroundN = 1;}
+      this.background = '../assets/homeBackground/' + this.backgroundN + '.png';
+      this.delay();
+    }, 3000);
+  }
+
+  reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
   }
 }
